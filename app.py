3e86d0a395883a54b9a5bc140e8fba27e2dc7795
Abel <abelmathew1504@gmail.com>
2024-12-13 10:28:13 +0530

    Chat completion API call functionality added

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, jsonify
 from openai import OpenAI
+import os
 
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
-def create_app():
-    app = Flask(__name__)
+client = OpenAI()
 
-    client = OpenAI()
+app = Flask(__name__)
 
-    @app.route("/", methods=['GET','POST'])
-    @app.route("/<name>", methods=['GET','POST'])
-    def prompt(name=None):
-        if request.method == 'POST':
-            error = None
-            prompt = request.form['prompt']
-            if not prompt:
-                error = "A prompt is required!"
-            if error is not None:
-                send_prompt(prompt)
-            flash(error)
-        return render_template("index.html",person=name)
+chat_history = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
 
-    return app
+@app.route("/", methods=['GET','POST'])
+@app.route("/<name>", methods=['GET','POST'])
+def index(name=None):
+    return render_template("index.html",person=name, chat_history=chat_history)
+
+@app.route("/prompt", methods=["POST"])
+def send_prompt():
+    content = request.json["message"]
+    chat_history.append({"role":"user","content": content})
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": content}
+    ]
+    )
+
+    text_content = None
+    response = completion.choices[0]
+    text_content = response.message.content
+    if text_content:
+        chat_history.append({"role":"assistant","content":text_content})
+        return jsonify(success=True,message=text_content)
+    else:
+        return jsonify(success=False,message="No text content found")
